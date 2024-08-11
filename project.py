0771086915b5a53500e6e2a86567f981a1c4ad75
Ethan Van De Woestyne <ethan.van@protonmail.com>
2024-08-11 00:19:15 -0600

    Adds constructor to Doctor class

---
+++ project.py
@@ -0,0 +1,8 @@
+class Doctor:
+    def __init__(self, doctor_id, name, specialization, worktime, qualification, room_number):
+        self.doctor_id = doctor_id
+        self.name = name
+        self.specialization = specialization
+        self.worktime = worktime
+        self.qualification = qualification
+        self.room_number = room_number
